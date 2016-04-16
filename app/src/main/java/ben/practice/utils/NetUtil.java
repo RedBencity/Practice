fb31355e8264f440fb641fd9d4a719a2b66d3fc6
RedBencity <375725096@qq.com>
2016-04-16 22:21:20 +0800

    ben

--- app/src/main/java/ben/practice/utils/NetUtil.java
+++ app/src/main/java/ben/practice/utils/NetUtil.java
@@ -4,14 +4,14 @@ package ben.practice.utils;
  * Created by dell on 2015-10-1.
  */
 public class NetUtil {
-//    final  static public String IP = "10.219.1.175";
 final  static public String IP = "42.96.176.131";
     //模拟器访问
 //    final  static public String URL = "http://10.219.1.175:8080/PracticeServer";
 //    final  static public String URL = "http://192.168.56.1:8080/PracticeServer";
     //手机访问
     final  static public String URL = "http://"+IP+":8080/PracticeServer";
-    final static public String downloadApk_URL = "http://"+IP+":8080/PracticeServer/version/app-release.apk";
+//    final static public String downloadApk_URL = "http://"+IP+":8080/PracticeServer/version/app-release.apk";
+    final static public String downloadApk_URL = "http://42.96.176.131:8080/PracticeServer/version/app-release.apk";
 
     //历史上的今天
     public static String getHistoryURL(String date){
